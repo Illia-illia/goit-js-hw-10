c9a37b659b5ff05cdb802bceaef470b400dff316
Illia-illia <97627728+Illia-illia@users.noreply.github.com>
2022-08-25 16:33:21 +0000

    Deploying to gh-pages from @ Illia-illia/goit-js-hw-10@ca2e819ff079da726fafc2c59c4af0ba5228cfdd 🚀

--- index.5c5ddf65.js
+++
@@ -1,2 +0,0 @@
-console.log(fetch("https://restcountries.com/v2/name/".concat("cana")).then((function(t){if(!t.ok)throw new Error(t.status);return t.json()})));
-//# sourceMappingURL=index.5c5ddf65.js.map
