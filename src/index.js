bb0bfe08518ff51f68d829cae3c24260db3e83bb
Illia-Illia <Takoexerrorx@gmail.com>
2022-08-25 20:30:52 +0300

    +-

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
 const inputEl = document.querySelector('#search-box');
 const listEl = document.querySelector('.country-list');
 const blockEl = document.querySelector('.country-info');
