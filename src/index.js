f2ab5c0248a5721b9184f0e44655f505b6c4652a
Illia-Illia <Takoexerrorx@gmail.com>
2022-08-25 16:22:16 +0300

    add get

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v2/name/${name}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+console.log(fetchCountries('cana'));
+
+// fetchCountries(name);
